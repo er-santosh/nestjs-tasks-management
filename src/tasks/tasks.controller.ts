5420740632a6ae0b8887c124310f59287146556d
Er Santosh <er.santoshtharu@gmail.com>
2022-06-06 14:28:31 +0545

    nestjs tasks management- get tasks and create task added

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,20 @@
+import { Task } from './task.model';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.taskService.createTask(title, description);
+  }
+}
